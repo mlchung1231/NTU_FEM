d4e1c79209a5e6edaac13474328e743c72c84822
mlchung1231 <L35104132@gmail.com>
2024-09-29 00:23:35 +0800

    hw2 with fig

--- HW2/p3_p4/HW2_3.m
+++ HW2/p3_p4/HW2_3.m
@@ -8,5 +8,26 @@ Stress_val = zeros(1,nelem);
 for i = 1:nelem
     Stress_val(i) = sqrt(sum(Stress_glob{i}(:).^2));  
 end
-max_stress = max(Stress_val);
-max_stress = max_stress/5;
+Stress_val = Stress_val./5;
+[max_stress, max_index] = max(Stress_val);
+
+
+figure;
+hold on;
+default_color = [0.3, 0.3, 0.3]; 
+max_stress_color = [1, 0, 0];  
+
+for i = 1:size(conn, 2)
+    triangle_vertices = coor(:, conn(:, i));
+    if i == max_index
+        patch('Vertices', triangle_vertices', 'Faces', [1, 2, 3], 'FaceColor', max_stress_color, 'EdgeColor', 'none');
+    else
+        patch('Vertices', triangle_vertices', 'Faces', [1, 2, 3], 'FaceColor', default_color, 'EdgeColor', 'none');
+    end
+end
+
+hold off;
+axis equal;
+xlabel('X-axis');
+ylabel('Y-axis');
+title('Maximum Stress Position');
